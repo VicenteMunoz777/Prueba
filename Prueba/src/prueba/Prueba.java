1225f141121edaef650d82fd044e4ab4c939a24f
VicenteMunoz777 <56983060+VicenteMunoz777@users.noreply.github.com>
2019-10-25 02:20:00 +0200

    Update Prueba.java

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -16,6 +16,8 @@ public class Prueba {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Adios mundo Cruel");
+        System.out.println("Profe pongame un 6 UwU");
     }
     
 }
